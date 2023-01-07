839286295de405f3cf639c31582ea5244605eba6
shivi6147 <42615654+shivi6147@users.noreply.github.com>
2023-01-07 21:29:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ const port = process.env.PORT;
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
-});
+const io = new Server(server);
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
